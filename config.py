5f2871b2c9d1925b9c2e382995672ca69ae8b584
Rahul R <68368287+Rahul-1315@users.noreply.github.com>
2021-05-21 20:09:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    SECRET_KEY = 'w9z$C&F)J@NcRfUjWnZr4u7x!A*G-KaPdSgVkYp2s5v8y/B?E(H+MbQeThWmZq'
+
+    MONGODB_SETTINGS = {'db': 'News_App'}
---
+++ main.py
@@ -0,0 +1,3 @@
+from application import app
+# For running the requirement.txt file
+#   pip install -r requirement.txt to install all packages!
